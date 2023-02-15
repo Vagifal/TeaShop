6f5d2bcd6ebb082f8035df7aa1fa9273a7bba213
Emin Fataliyev <EmileWin@users.noreply.github.com>
2023-02-15 19:29:44 +0200

    adding login & logout routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -54,6 +54,54 @@ const userController = {
             return res.status(500).json({ message: error.message })
         }
     },
+    login: async (req, res) => {
+        try {
+            const { email, password } = req.body;
+
+            const user = await Users.findOne({ email })
+            if (!user)
+                return res.status(400).json({ message: "User not found" })
+
+            const isMatch = await bcrypt.compare(password, user.password)
+            if (!isMatch)
+                return res.status(400).json({ message: "Incorrect password" })
+
+            const accessToken = createAccessToken({ id: user._id })
+            const refreshToken = createRefreshToken({ id: user._id })
+
+            res.cookie('refreshToken', refreshToken, {
+                httpOnly: true,
+                path: '/user/refreshToken',
+                //maxAge: 7 * 24 * 60 * 60 * 1000 // 7d
+            })
+
+            res.json({ accessToken })
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    },
+    logout: async (req, res) => {
+        try {
+            res.clearCookie('refreshToken', { path: '/user/refreshToken' })
+            return res.json({ message: "Logged out" })
+
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    },
+    getUser: async (req, res) => {
+        try {
+            const user = await Users.findById(req.user.id).select('-password')
+
+            if (!user)
+                return res.status(400).json({ message: "User does not exist" })
+
+            res.json(user)
+        } catch (error) {
+            return res.status(500).json({ message: error.message })
+        }
+    }
 }
 
 const createAccessToken = (user) => jwt.sign(user, "" + process.env.ACCESS_TOKEN, { expiresIn: '1d' })
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,8 +1,15 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
+const auth = require('../middleware/auth')
 
 router.post('/register', userController.register)
 
+router.post('/login', userController.login)
+
+router.get('/logout', userController.logout)
+
 router.get('/refreshToken', userController.refreshToken)
+
+router.get('/info', auth, userController.getUser)
 
 module.exports = router
